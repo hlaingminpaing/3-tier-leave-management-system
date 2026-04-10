05db993e375f03ce1192a8fcb4c7178d047d385b
Hlaing Min Paing <hlaingminpaing.ygn@gmail.com>
2026-04-11 00:20:02 +0700

    fix:update api url1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,5 +5,5 @@ AWS:
   http://<BACKEND-ALB-DNS>
 leave-system-alb-1763611467.ap-southeast-7.elb.amazonaws.com
 */
-const API_URL = "http://leave-system-alb-1763611467.ap-southeast-7.elb.amazonaws.com";
+export const API_URL = "http://leave-system-alb-1763611467.ap-southeast-7.elb.amazonaws.com";
 // export const API_URL = globalThis.env?.API_URL || import.meta.env.VITE_API_URL || "/api";
