ede016c6e1185ab2e59f4529644bcc4ec1dd3a6b
Hlaing Min Paing <50479345+hlaingminpaing@users.noreply.github.com>
2026-04-11 00:11:42 +0700

    Update API_URL with AWS ALB DNS
    Updated API_URL to include AWS ALB DNS.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,5 +3,7 @@ LOCAL:
   http://localhost:3000
 AWS:
   http://<BACKEND-ALB-DNS>
+leave-system-alb-1763611467.ap-southeast-7.elb.amazonaws.com
 */
-export const API_URL = globalThis.env?.API_URL || import.meta.env.VITE_API_URL || "/api";
+leave-system-alb-1763611467.ap-southeast-7.elb.amazonaws.com
+// export const API_URL = globalThis.env?.API_URL || import.meta.env.VITE_API_URL || "/api";
